cc361633f11ec56c73e38f5af92a70048fa73a9b
JennyCraig74 <101858941@swin.edu.au>
2021-04-10 11:36:44 +1000

    3rd commit

---
+++ RepetitionTasks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace repetitiontasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
