db427bb35be53f4ab364759010edf436d0bad64c
JennyCraig74 <101858941@swin.edu.au>
2021-04-13 20:21:13 +1000

    Variable Task Done

--- Selection/Program.cs
+++ Selection/Program.cs
@@ -7,6 +7,8 @@ namespace Selection
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
         }
     }
 }
--- Variables/Program.cs
+++ Variables/Program.cs
@@ -6,7 +6,32 @@ namespace Variables
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Enter FirstName");
+            string FirstName = Console.ReadLine();
+            Console.WriteLine(FirstName); 
+
+            Console.WriteLine("Enter SurName");
+            string SurName = Console.ReadLine();
+            Console.WriteLine(SurName);  
+
+            Console.WriteLine("Enter ID ");
+            string ID = Console.ReadLine();
+            Console.WriteLine(ID);  
+
+            Console.WriteLine("Enter YOB ");
+            int YOB = int.Parse(Console.ReadLine());
+            Console.WriteLine(YOB); 
+
+            Console.WriteLine("Please Enter Current Year");
+            int CurrentYear =int.Parse(Console.ReadLine());
+            Console.WriteLine(CurrentYear); 
+            
+            Console.WriteLine("Your name is " + FirstName + " " + SurName + " Your Age Is " + (CurrentYear - YOB) ); 
+
+            Console.ReadLine();
         }
     }
 }
+
+
